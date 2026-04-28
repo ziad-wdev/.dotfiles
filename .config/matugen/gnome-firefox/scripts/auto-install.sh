4ca9d74b269c4a6ed63d74cf7d3b0a12a20b9cc6
ziad-wdev <ziadahmed2371@gmail.com>
2026-04-28 17:25:22 +0300

    main

---
+++ .config/matugen/gnome-firefox/scripts/auto-install.sh
@@ -0,0 +1,46 @@
+#! /usr/bin/env bash
+
+firefoxInstallationPaths=(
+    # Firefox
+    ~/.mozilla/firefox # Package
+    "${XDG_CONFIG_HOME:-$HOME/.config}/mozilla/firefox" # Package (XDG)
+    ~/.var/app/org.mozilla.firefox/.mozilla/firefox # Flatpak
+    ~/snap/firefox/common/.mozilla/firefox # Snap
+    "$HOME/Library/Application Support/Firefox" # MacOS Package
+    ~/AppData/Roaming/Mozilla/Firefox # Microsoft Windows
+
+    # Librewolf
+    ~/.librewolf # Package
+    "${XDG_CONFIG_HOME:-$HOME/.config}/librewolf/librewolf" # Package (XDG)
+    ~/.var/app/io.gitlab.librewolf-community/.librewolf # Flatpak
+
+    # Floorp
+    ~/.floorp # Package
+    ~/.var/app/one.ablaze.floorp/.floorp # Flatpak
+
+    # Waterfox
+    ~/.var/app/net.waterfox.waterfox/.waterfox # Flatpak
+    
+)
+
+installScript="./scripts/install.sh"
+folderArg=""
+foldersFoundCount=0
+
+eval "chmod +x ${installScript}"
+
+for folder in "${firefoxInstallationPaths[@]}"; do
+    if [ -d "$folder" ]; then
+    echo Firefox installation folder found
+
+    folderArg=" -f \"$folder\""
+    eval ${installScript}${folderArg}   
+    foldersFoundCount+=1
+
+    fi
+
+done
+
+if [ $foldersFoundCount = 0 ];then
+    echo No firefox folder found ;
+    fi
