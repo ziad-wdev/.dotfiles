d11f057aa8f5c254bd7c355a5bbd64bb612d631f
ziad-wdev <ziadahmed2371@gmail.com>
2026-05-09 03:56:01 +0300

    main

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,21 +22,4 @@ function qet
     pacman -Qetq | sort
 end
 
-function wal
-    if test -f "$argv[1]"
-        # 1. Set wallpaper
-        awww img "$argv[1]" --transition-type random
-        
-        # 2. Sync with Matugen
-        matugen image "$argv[1]" --prefer lightness
-        
-        # 3. Update symlink for hyprlock
-        ln -sf (realpath "$argv[1]") /tmp/current_wallpaper.png
-        
-        echo "Wallpaper and theme updated successfully!"
-    else
-        echo "Usage: change_wallpaper <path_to_image>"
-    end
-end
-
 fish_add_path /home/ziad/.spicetify
